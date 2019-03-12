179885e7997b94709a30bb0449183b15faa6b4f4
Michael <msiller455@gmail.com>
2019-03-12 13:24:28 -0700

    adds post route to events routes

--- models/events.js
+++ models/events.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const eventSchema = mongoose.Schema({
     "county_region": String,
     "site_name": String,
-    
+    "created_by": String,
+    "users": [{type: Schema.Types.ObjectId, ref: 'User'}]
 })
 
 const Event = mongoose.model('Event', eventSchema)
